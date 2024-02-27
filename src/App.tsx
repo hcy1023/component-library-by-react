103119ee167a10c902d1ba2099868167a88d0cd5
HcY <1462984578@qq.com>
2024-02-27 15:40:28 +0800

    feat: ColorPicker init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MiniCalendar from "./component/MiniCalendar";
 import Calendar from "./component/Calendar";
+import ColorPickerPanel from "./component/Colorpicker/ColorPickerPanel";
 import dayjs from "dayjs";
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
         {/*<MiniCalendar value={new Date('1999-10-23')} onChange={(date: Date) => {*/}
         {/*    alert(date.toLocaleDateString())*/}
         {/*}}/>*/}
-        <Calendar value={dayjs('2024-02-21')} locale="en-US" onChange={(date) => {
-            alert(date.format('YYYY-MM-DD'));
-        }}/>
+        {/*<Calendar value={dayjs('2024-02-21')} locale="en-US" onChange={(date) => {*/}
+        {/*    alert(date.format('YYYY-MM-DD'));*/}
+        {/*}}/>*/}
+        <ColorPickerPanel></ColorPickerPanel>
     </div>
   );
 }
